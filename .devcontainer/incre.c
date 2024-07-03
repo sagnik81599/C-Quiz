f7dd5de6d0bd12a19ccee4ef083ca3f12e3ab41f
SAGNIK SAHA <126655769+sagnik81599@users.noreply.github.com>
2024-07-03 06:08:41 +0000

    changes

---
+++ .devcontainer/incre.c
@@ -0,0 +1,51 @@
+// #include<stdio.h>
+// void main(){
+//     int x=10;
+//     int y=x++;
+//     printf("%d ",y);
+//     printf("%d\n",x);
+// }
+
+// Ans: 10,11
+// -------------------------------------------
+
+// #include<stdio.h>
+//  void main(){
+//     int x=5;
+//     int y=x++*3;
+//     printf("%d %d \n",x,y);
+//  }
+
+//  Ans:- 6,15
+//  -------------------------------------------
+
+// #include<stdio.h>
+//  void main(){
+//     int x=10;
+//     int y= ++x + x++;
+//     printf("%d %d \n",x,y);
+//  }
+
+//  Ans:- 12,22
+//  -----------------------------------------
+
+// #include<stdio.h>
+//  void main(){
+//     int x=5;
+//     printf("%d ",++x);
+//     printf("%d ",x++);
+//     printf("%d \n",x);
+//  }
+
+//  Ans:- 6,6,7
+// -----------------------------------
+
+// #include<stdio.h>
+//  void main(){
+//     int a=2,b=3,c;
+//     c=a++ + ++b + ++a + b++;
+//     printf("%d %d %d \n",a,b,c);
+// }
+
+// Ans:- 4,5,14
+// -----------------------------------
