fc306efa9cec007e11e68183bb16e9630ca09203
SAGNIK SAHA <126655769+sagnik81599@users.noreply.github.com>
2024-07-07 05:44:44 +0000

    changes

---
+++ .devcontainer/swapno.c
@@ -0,0 +1,37 @@
+// #include<stdio.h>
+// void main(){
+//     int a,b;
+//     printf("Enter the frist number: ");
+//     scanf("%d",&a);
+//     printf("Enter the second number: ");
+//     scanf("%d",&b);
+
+//     // no third variable use swap no.
+//     a = a+b;// a=130  (90+40)
+//     b = a-b;// b=40   (130-90)
+//     a = a-b;// a=90   (130-40)
+
+//     printf("a=%d\nb=%d\n",a,b);
+
+// }
+
+//--------------------------------------------------------------------
+
+
+// #include<stdio.h>
+// void main(){
+//     int a,b;
+//     int swap;
+//     printf("Enter the frist number: ");
+//     scanf("%d",&a);
+//     printf("Enter the second number: ");
+//     scanf("%d",&b);
+
+//     // using third variable use swap no.
+//     swap=a;
+//     a=b;
+//     b=swap;
+
+//     printf("a=%d\nb=%d\n",a,b);
+
+// }
