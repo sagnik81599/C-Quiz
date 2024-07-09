a5b5f5868929ec2f7f48abd86ee93e1171b6da2f
SAGNIK SAHA <126655769+sagnik81599@users.noreply.github.com>
2024-07-09 14:30:12 +0000

    changes

--- .devcontainer/func.c
+++ .devcontainer/func.c
@@ -1,20 +1,20 @@
-#include<stdio.h>
-void mul(int x,int y);
-int main(){
- int a,b;
- printf("Enter the number :");
- scanf("%d",&a);
- printf("Enter the second number :");
- scanf("%d",&b);
- mul(a,b);
-}
+// #include<stdio.h>
+// void mul(int x,int y);
+// int main(){
+//  int a,b;
+//  printf("Enter the number :");
+//  scanf("%d",&a);
+//  printf("Enter the second number :");
+//  scanf("%d",&b);
+//  mul(a,b);
+// }
 
-void mul(int x,int y){
-    int t= x+y;
-    int v=x-y;
-    printf("SUM is :%d\n",t);
-    printf("subtraction is :%d\n",v);
-}
+// void mul(int x,int y){
+//     int t= x+y;
+//     int v=x-y;
+//     printf("SUM is :%d\n",t);
+//     printf("subtraction is :%d\n",v);
+// }
 
 // Ans:- Enter the number :40
 // Enter the second number :30
@@ -22,3 +22,29 @@ void mul(int x,int y){
 // subtration is :10
 //--------------------------------------------------------------------
 
+// using pointer swap two number
+// #include<stdio.h>
+// void swap(int *x,int *y);
+// void main(){
+//     int a,b;
+//     printf("Enter the number :");
+//     scanf("%d",&a);
+//     printf("Enter the number :");
+//     scanf("%d",&b);
+//     swap(&a,&b); 
+//     printf("a=%d\nb=%d\n",a,b);  
+// }
+
+// void swap(int *x,int *y){
+//     int t;
+//     t=*x;
+//     *x=*y;
+//     *y=t;
+   
+// }
+
+// Enter the number :40
+// Enter the number :70
+// a=70
+// b=40
+// --------------------------------------------------------
