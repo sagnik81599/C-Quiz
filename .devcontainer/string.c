fbb715f61c27cc2848e8bf09c32ddeaf2b68608b
SAGNIK SAHA <126655769+sagnik81599@users.noreply.github.com>
2024-07-15 14:28:32 +0000

    changes

--- .devcontainer/string.c
+++ .devcontainer/string.c
@@ -1,5 +1,20 @@
-#include<stdio.h>
-int main(){
-char str[] ="HELLO SAGNIK";
-printf("%s\n",str);
-}
+// #include<stdio.h>
+// int main(){
+// char str[] ="HELLO SAGNIK";
+// printf("%s\n",str);
+// }
+
+// Ans:- HELLO SAGNIK
+//----------------------------------------------------
+
+// #include<stdio.h>
+// #include<string.h>
+// int main(){
+//    char str1[20]="HELLO ";
+//    char str2[20]="WORLD";
+
+//     printf("%s\n",strcat(str1,str2));
+// }
+
+//Ans:- HELLO WORLD
+//------------------------------------------------------------
