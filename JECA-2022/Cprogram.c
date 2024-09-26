2b7bc7828a2f82d83afb8e597daf9e990f1cf4b8
SAGNIK SAHA <126655769+sagnik81599@users.noreply.github.com>
2024-09-26 05:41:06 +0000

    changes

---
+++ JECA-2022/Cprogram.c
@@ -0,0 +1,25 @@
+// #include<stdio.h>
+// void main(){
+//     int  a=4;
+//     float b;
+//     b= ++a;
+//     printf("%d %f\n",a,++b);
+// }
+
+// ANS:- 5,6.000
+//-----------------------------------------
+
+
+// #include<stdio.h>
+// void main(){
+  
+// int *j;
+// {
+//     int i = 1000;
+//     j=&i;
+// }
+// printf("%d",*j);
+// }
+
+// Ans:- 1000
+
